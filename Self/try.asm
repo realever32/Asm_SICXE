5e2890561335ae9fee76678291b3740edc1190b7
k1tten <realever15@gmail.com>
2015-11-15 20:56:14 +0800

    practice

--- Self/try.asm
+++ Self/try.asm
@@ -1,11 +1,27 @@
 try 	START	0
-first	LDCH S0,X
-	STCH S1,X
-	TIX S3
-	JLT first
-
-S0	BYTE C'test string'
-S1	RESB 11
-S3	WORD 11
-	END first
-
+first	LDA S1
+	ADD 5
+	STA S1
+	LDA S2
+	ADD 4
+	STA S2
+	LDA S3
+	ADD 1
+	STA S3
+	LDA S1
+	COMP S2
+	RD inpdev
+	JGT print
+	JLT adder
+print	TD outdev
+	JEQ print
+	WD  outdev
+adder	LDA S1
+	ADD S3
+	STA S1
+outdev 	BYTE X'05'
+inpdev	BYTE X'F1'	
+S1	RESW 1
+S2	RESW 1
+S3	RESW 1
+	END first 
