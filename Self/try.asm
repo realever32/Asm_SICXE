364322a1e5a826f0cf4f624be73d2f0e2a7a908e
k1tten <realever15@gmail.com>
2015-11-23 10:28:00 +0800

    class practice try.asm

--- Self/try.asm
+++ Self/try.asm
@@ -1,27 +1,16 @@
-try 	START	0
-first	LDA S1
-	ADD 5
-	STA S1
-	LDA S2
-	ADD 4
-	STA S2
-	LDA S3
-	ADD 1
-	STA S3
-	LDA S1
-	COMP S2
-	RD inpdev
-	JGT print
-	JLT adder
-print	TD outdev
-	JEQ print
-	WD  outdev
-adder	LDA S1
-	ADD S3
-	STA S1
-outdev 	BYTE X'05'
-inpdev	BYTE X'F1'	
-S1	RESW 1
-S2	RESW 1
-S3	RESW 1
-	END first 
+PRACT	START	0
+INLOOP	TD 	INDEV
+	JEQ	INLOOP
+	RD	INDEV
+	STCH	DATA
+.
+OUTLP	TD	OUTDEV
+	JEQ	OUTLP
+	LDCH	DATA
+	WD	OUTDEV
+.
+INDEV	BYTE	X'F1'
+OUTDEV	BYTE	X'05'
+DATA	WORD	C'G'	
+	END	INLOOP
+
